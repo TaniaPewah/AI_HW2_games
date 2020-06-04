48078bc56e3bee8282e107fbeb15150d0fae8c1e
Tania <drob.tania@gmail.com>
2020-06-04 20:49:35 +0300

    not working

--- MinimaxPlayer.py
+++ MinimaxPlayer.py
@@ -1,3 +1,5 @@
+import time as tm
+
 class MinimaxPlayer:
     def __init__(self):
         self.loc = None
@@ -12,8 +14,63 @@ class MinimaxPlayer:
                     self.loc = (i, j)
                     break
 
-    def make_move(self, time):  # time parameter is not used, we assume we have enough time.
-        return True
+    def make_move(self, time_limit): #number of seconds to finish
+        start_time = tm.time()
+
+        return move
+
+'''
+        start = time.time()
+
+        # Collect legal moves and successor states
+        legalMoves = gameState.getLegalActions()
+
+        # find the next agent modulo the number of agents
+        next_agent = 1 % gameState.getNumAgents()
+
+        # update the depth if we finished a full round of turns
+        # pacmang, ..., last ghost
+        if next_agent == 0:
+            next_depth = self.depth - 1
+        else:
+            next_depth = self.depth
+        # according to staff we can assume that the game won't be launched with
+        # 0 ghosts and with depth=0 therefor next_depth >= 0
+
+        # Choose one of the best actions
+        scores = [self.rbMinimax(gameState.generatePacmanSuccessor(action), \
+                next_agent, next_depth) for action in legalMoves]
+        bestScore = max(scores)
+        bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
+        chosenIndex = random.choice(bestIndices) # Pick randomly among the best
+
+        end = time.time()
+
+        # update meta data
+        self.time_total_actions += (end-start)
+        self.num_actions += 1
+
+        return legalMoves[chosenIndex]
+'''
+
+'''
+        move, = (s,)
+        last_iteration_time = tm.time() - ID_start_time
+        next_iteration_max_time = (, last_iteration_time)
+        time_until_now = tm.time() - start_time
+        While
+        time_until_now + next_iteration_max_time < time_limit:
+        8 += 1
+        iteration_start_time = tm.time()
+
+        move, = (s,)
+        last_iteration_time = tm.time() - iteration_start_time
+        next_iteration_max_time = (, last_iteration_time)
+        time_until_now = tm.time() - ID_start_time
+        
+'''
+
+
 
     def set_rival_move(self, loc):
         return True
